e485bf85c12c2814ba45ab81a0704c1a6ed78f2a
Jakob <jakobverlic66@gmail.com>
2020-11-21 20:46:40 +0100

    going to try a different way of making a registration form

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.login_page),
     path('submitlogin/', views.submit_login),
     path('submitlogout/', views.submit_logout),
-    path('adduser/', views.add_user_page)
+    path('adduser/', views.add_user_page),
+    path('adduser/submit/', views.add_user)
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -31,3 +31,10 @@ def submit_logout(request):
 @login_required
 def add_user_page(request):
     return render(request, 'accounts/add_user.html', {'add_user_page': True})
+
+
+@login_required
+def add_user(request):
+    username = request.POST['username']
+    is_admin = request.POST ['is_admin']
+    return print(is_admin)
