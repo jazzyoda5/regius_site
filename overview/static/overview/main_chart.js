304dfa2c0ae333c207c25d4b368a8b47328d12e8
Jakob <jakobverlic66@gmail.com>
2020-12-10 19:19:32 +0100

    added a gantt chart to overview

--- overview/static/overview/main_chart.js
+++ overview/static/overview/main_chart.js
@@ -1,69 +1,127 @@
-var barOptions_stacked = {
-  hover: {
-    animationDuration: 10,
-  },
-  scales: {
-    xAxes: [
-      {
-        label: "Duration",
-        ticks: {
-          beginAtZero: true,
-          fontFamily: "'Open Sans Bold', sans-serif",
-          fontSize: 11,
-        },
-        scaleLabel: {
-          display: false,
-        },
-        gridLines: {},
-        stacked: true,
-      },
-    ],
-    yAxes: [
-      {
-        gridLines: {
-          display: false,
-          color: "#fff",
-          zeroLineColor: "#fff",
-          zeroLineWidth: 0,
-        },
-        ticks: {
-          fontFamily: "'Open Sans Bold', sans-serif",
-          fontSize: 11,
-        },
-        stacked: true,
-      },
-    ],
-  },
-  legend: {
-    display: false,
-  },
-};
+var endpoint = "/overview/convert/projectsdata/";
+var labels = [''];
+var start_dates = [''];
+var end_dates = [''];
 
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-  type: "horizontalBar",
-  data: {
-    labels: ["1", "2", "3", "4"],
-
-    datasets: [
-      {
-        data: [50, 150, 300, 400, 500],
-        backgroundColor: "rgba(63,103,126,0)",
-        hoverBackgroundColor: "rgba(50,90,100,0)",
-      },
-      {
-        data: [100, 100, 200, 200, 100],
-        backgroundColor: ["red", "green", "blue", "yellow"],
-      },
-    ],
+$.ajax({
+  method: "GET",
+  url: endpoint,
+  success: function (data) {
+    for (let i in data) {
+      labels.push(data[i].name);
+      start_dates.push(new Date(data[i].start_date));
+      end_dates.push(new Date(data[i].end_date));
+    }
+    console.log(labels);
+    console.log(start_dates);
+    console.log(end_dates);
+    drawChart();
   },
-  options: barOptions_stacked,
+  error: function (error_data) {
+    console.log("error");
+    console.log(error_data);
+  },
 });
 
-// this part to make the tooltip only active on your real dataset
-var originalGetElementAtEvent = myChart.getElementAtEvent;
-myChart.getElementAtEvent = function (e) {
-  return originalGetElementAtEvent.apply(this, arguments).filter(function (e) {
-    return e._datasetIndex === 1;
+function drawChart() {
+  var timeFormat = "DD/MM/YYYY";
+
+  // Helper functions 
+  /*
+  function newDate(days) {
+    return moment().add(days, "d").toDate();
+  } */
+
+  // Function to parse the dates so ChartJS can read them
+  function newDateString(days) {
+    return moment().add(days, "d").format(timeFormat);
+  }
+
+  /*
+This function calculates how many days 
+the date from django is form today
+*/
+  function getDaysFromToday(date) {
+    var date1 = new Date(date);
+    var date2 = new Date();
+    var difference = date1.getTime() - date2.getTime();
+    var days = Math.ceil(difference / (1000 * 3600 * 24));
+    return Number(days);
+  }
+
+  var ctx = document.getElementById("chart1").getContext("2d");
+
+  // Get datasets for the chart
+  function getDatasets() {
+    console.log('start_dates[0]', start_dates[1]);
+    let datasets = [];
+
+    for (let i = 0; i <= labels.length; i++) {
+      let days1 = getDaysFromToday(start_dates[i]);
+      let days2 = getDaysFromToday(end_dates[i]);
+
+      let set = {
+        label: "Ime projekta",
+        backgroundColor: "rgba(246,156,85,4)",
+        borderColor: "rgba(246,156,85,4)",
+        fill: false,
+        borderWidth: 15,
+        pointRadius: 0,
+        data: [
+          {
+            x: newDateString(days1),
+            y: labels[i],
+          },
+          {
+            x: newDateString(days2),
+            y: labels[i],
+          },
+        ],
+      };
+
+      datasets.push(set);
+    }
+    console.log("datasets: ", datasets);
+    return datasets;
+  }
+
+  var chart_datasets = getDatasets();
+
+  // Chart
+  var scatterChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      yLabels: labels,
+      datasets: chart_datasets,
+    },
+    options: {
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [
+          {
+            type: "time",
+            time: {
+              parser: timeFormat,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            type: "category",
+            position: "left",
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: "Projekti",
+            },
+            ticks: {
+              reverse: true,
+            },
+          },
+        ],
+      },
+    },
   });
-};
+}
--- overview/views.py
+++ overview/views.py
@@ -8,9 +8,19 @@ from django.http import JsonResponse
 def overview_page(request):
     # Get all current projects
     projects = Project.objects.filter(status='V teku')
+    labels = []
+    start_dates = []
+    end_dates = []
+    for project in projects:
+        labels.append(project.project_name)
+        start_dates.append(project.project_start_date)
+        end_dates.append(project.project_end_date)
     print(projects)
     context = {
-        'username': request.user
+        'username': request.user,
+        'labels': labels,
+        'start_dates': start_dates,
+        'end_dates': end_dates
     }
     return render(request, 'overview/main.html', context)
 
@@ -19,7 +29,7 @@ def overview_page(request):
 def homepage_json_data(request):
 
     # Get all current projects
-    projects = Project.objects.filter(status='V teku')
+    projects = Project.objects.filter(status='V teku').order_by('project_start_date')
     print(projects)
 
     data = {
